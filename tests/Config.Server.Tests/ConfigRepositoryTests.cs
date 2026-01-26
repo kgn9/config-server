cc12a18d434b96b7adf01fbdff491a4800111d2f
kgn9 <igornasonkov@gmail.com>
2026-01-26 08:24:08 +0300

    test: added tests - Added tests for ConfigService and ConfigRepository

---
+++ tests/Config.Server.Tests/ConfigRepositoryTests.cs
@@ -0,0 +1,208 @@
+﻿#pragma warning disable CS8618
+
+using Config.Server.Application.Abstractions.Queries;
+using Config.Server.Application.Models.Entities;
+using Config.Server.Application.Models.Enums;
+using Config.Server.Infrastructure.Persistence.Repositories;
+using FluentAssertions;
+using Npgsql;
+using System.Data;
+using Testcontainers.PostgreSql;
+
+namespace Config.Server.Tests;
+
+public class ConfigRepositoryTests : IAsyncLifetime
+{
+    private const string ConnectionString = "Host=localhost;Port=5433;Database=test-database;Username=postgres;Password=postgres";
+
+    private PostgreSqlContainer _container;
+    private NpgsqlDataSource _dataSource;
+    private ConfigRepository _configRepository;
+
+    public async Task InitializeAsync()
+    {
+        _container = new PostgreSqlBuilder(image: "postgres:latest")
+            .WithUsername("postgres")
+            .WithPassword("postgres")
+            .WithDatabase("test-database")
+            .WithPortBinding(5433, 5432)
+            .Build();
+        await _container.StartAsync();
+
+        var builder = new NpgsqlDataSourceBuilder(ConnectionString);
+        builder.MapEnum<ConfigEnvironment>(pgName: "config_environment");
+        _dataSource = builder.Build();
+
+        await InitializeDatabaseSchemaAsync();
+
+        _configRepository = new ConfigRepository(_dataSource);
+    }
+
+    public async Task DisposeAsync()
+    {
+        await _container.DisposeAsync();
+        await _dataSource.DisposeAsync();
+    }
+
+    [Fact]
+    public async Task AddOrUpdateAsync_ShouldInsert_WhenConfigIsNew()
+    {
+        // Arrange
+        ConfigItem item = new(
+            Id: 0,
+            "key1",
+            "value1",
+            "TestProject1",
+            "test1",
+            [ConfigEnvironment.Global],
+            DateTime.Today,
+            DateTime.Today,
+            "testCerator1");
+
+        const string sql = """
+        select count(*) from configurations
+        where key = 'key1';
+        """;
+
+        // Act
+        ConfigItem result = await _configRepository.AddOrUpdateConfigAsync(item,  CancellationToken.None);
+
+        // Assert
+        result.Id.Should().BeGreaterThan(0);
+
+        NpgsqlConnection conn = _dataSource.CreateConnection();
+        await conn.OpenAsync();
+        await using var command = new NpgsqlCommand(sql, conn);
+        long? rowsAffected = (long?)await command.ExecuteScalarAsync();
+        rowsAffected.Should().Be(1);
+    }
+
+    [Fact]
+    public async Task AddOrUpdateAsync_ShouldUpdate_WhenConfigExists()
+    {
+        // Arrange
+        ConfigItem existingItem = new(
+            Id: 0,
+            "existingKey",
+            "existingValue",
+            "TestProject1",
+            "test1",
+            [ConfigEnvironment.Global],
+            DateTime.Today,
+            DateTime.Today,
+            "testCerator1");
+
+        ConfigItem updatedItem = existingItem with { Value = "updatedValue", UpdatedAt = DateTime.Today.AddDays(1) };
+
+        existingItem = await _configRepository.AddOrUpdateConfigAsync(existingItem, CancellationToken.None);
+
+        const string sql = """
+        select * from configurations
+        where key = 'existingKey';
+        """;
+
+        // Act
+        updatedItem = await _configRepository.AddOrUpdateConfigAsync(updatedItem, CancellationToken.None);
+
+        // Assert
+        updatedItem.Id.Should().Be(existingItem.Id);
+
+        NpgsqlConnection conn = _dataSource.CreateConnection();
+        await conn.OpenAsync();
+        await using var command = new NpgsqlCommand(sql, conn);
+        NpgsqlDataReader reader = await command.ExecuteReaderAsync();
+        reader.Read().Should().BeTrue();
+        reader.GetDateTime("updated_at").Should().Be(updatedItem.UpdatedAt);
+        reader.GetString("value").Should().Be(updatedItem.Value);
+    }
+
+    [Fact]
+    public async Task QueryConfigsAsync_ShouldReturnCorrectConfigs_WhenConfigsArePresent()
+    {
+        // Arrange
+        ConfigItem item1 = new(
+            Id: 0,
+            "key1",
+            "value1",
+            "TestProject1",
+            "test1",
+            [ConfigEnvironment.Global],
+            DateTime.Today,
+            DateTime.Today,
+            "testCerator1");
+        ConfigItem item2 = new(
+            Id: 0,
+            "key2",
+            "value2",
+            "TestProject1",
+            "test1",
+            [ConfigEnvironment.Global],
+            DateTime.Today,
+            DateTime.Today,
+            "testCerator1");
+        ConfigItem item3 = new(
+            Id: 0,
+            "key3",
+            "value3",
+            "TestProject1",
+            "test1",
+            [ConfigEnvironment.Global],
+            DateTime.Today,
+            DateTime.Today,
+            "testCerator1");
+
+        item1 = await _configRepository.AddOrUpdateConfigAsync(item1, CancellationToken.None);
+        item2 = await _configRepository.AddOrUpdateConfigAsync(item2, CancellationToken.None);
+        item3 = await _configRepository.AddOrUpdateConfigAsync(item3, CancellationToken.None);
+        ConfigItem[] items = [item1, item2, item3];
+
+        ConfigQuery query = new(
+            ["key1", "key2", "key3"],
+            "TestProject1",
+            "test1",
+            ConfigEnvironment.Global,
+            50,
+            0);
+
+        // Act
+        IAsyncEnumerable<ConfigItem> result = _configRepository.QueryConfigsAsync(query, CancellationToken.None);
+
+        // Assert
+        ConfigItem[] receivedItems = await result.ToArrayAsync();
+        receivedItems.Should().BeEquivalentTo(items);
+    }
+
+    private async Task InitializeDatabaseSchemaAsync()
+    {
+        await using var setupDataSource = NpgsqlDataSource.Create(ConnectionString);
+        await using NpgsqlConnection connection = await setupDataSource.OpenConnectionAsync();
+        await using NpgsqlCommand command = connection.CreateCommand();
+
+        command.CommandText = """
+          create type config_environment as enum ('dev', 'stage', 'prod', 'global');
+          
+          create table configurations
+          (
+              id          bigint primary key generated always as identity,
+          
+              key         text,
+              value       text not null,
+          
+              namespace   text not null,
+              profile     text not null default 'default',
+              environment config_environment[] not null,
+          
+              created_at  timestamp with time zone not null default now(),
+              updated_at  timestamp with time zone not null default now(),
+              created_by  text not null,
+          
+              is_deleted  bool default false,
+          
+              constraint unique_config_record
+                  unique (namespace, profile, environment, key)
+          );
+          """;
+
+        await command.ExecuteNonQueryAsync();
+    }
+}
---
+++ tests/Config.Server.Tests/ConfigServiceTests.cs
@@ -0,0 +1,324 @@
+﻿using Config.Server.Application.Abstractions.Queries;
+using Config.Server.Application.Abstractions.Repositories;
+using Config.Server.Application.Contracts.Operations;
+using Config.Server.Application.Models.Entities;
+using Config.Server.Application.Models.Enums;
+using Config.Server.Application.Services;
+using FluentAssertions;
+using NSubstitute;
+using System.Text.Json;
+
+namespace Config.Server.Tests;
+
+public class ConfigServiceTests
+{
+    private readonly IConfigRepository _configRepository;
+    private readonly IConfigHistoryRepository _configHistoryRepository;
+    private readonly ConfigService _configService;
+
+    public ConfigServiceTests()
+    {
+        _configRepository = NSubstitute.Substitute.For<IConfigRepository>();
+        _configHistoryRepository = NSubstitute.Substitute.For<IConfigHistoryRepository>();
+        _configService = new ConfigService(_configRepository, _configHistoryRepository);
+    }
+
+    [Fact]
+    public async Task GetConfigByKeyAsync_ReturnsSuccess_WhenConfigExists()
+    {
+        // Arrange
+        GetConfig.Request request = new("key1", "TestProject1", "test1", ConfigEnvironment.Global);
+        ConfigItem expected = new(
+            Id: 1,
+            "key1",
+            "value1",
+            "TestProject1",
+            "test1",
+            [ConfigEnvironment.Global],
+            DateTime.Today,
+            DateTime.Today,
+            "testCreator1");
+
+        _configRepository
+            .QueryConfigsAsync(
+                Arg.Is<ConfigQuery>(q => q.Keys.AsEnumerable().Contains("key1")),
+                Arg.Any<CancellationToken>())
+            .Returns(new[] { expected }.ToAsyncEnumerable());
+
+        // Act
+        GetConfig.Result result = await _configService.GetConfigByKeyAsync(request, CancellationToken.None);
+
+        // Assert
+        result.Should().BeOfType<GetConfig.Result.Success>()
+            .Which.ConfigItem.Should().BeEquivalentTo(expected);
+    }
+
+    [Fact]
+    public async Task GetConfigByKeyAsync_ReturnsNotFound_WhenConfigDoesNotExist()
+    {
+        // Arrange
+        GetConfig.Request request = new("key1", "TestProject1", "test1", ConfigEnvironment.Global);
+
+        _configRepository
+            .QueryConfigsAsync(Arg.Any<ConfigQuery>(), Arg.Any<CancellationToken>())
+            .Returns(AsyncEnumerable.Empty<ConfigItem>());
+
+        // Act
+        GetConfig.Result result = await _configService.GetConfigByKeyAsync(request, CancellationToken.None);
+
+        // Assert
+        result.Should().BeOfType<GetConfig.Result.NotFound>();
+    }
+
+    [Fact]
+    public async Task SetConfigAsync_CreatesConfigAndHistory_WhenNewConfigAdded()
+    {
+        // Arrange
+        ConfigItem item = new(
+            Id: default,
+            "key1",
+            "value1",
+            "TestProject1",
+            "test1",
+            [ConfigEnvironment.Global],
+            DateTime.Today,
+            DateTime.Today,
+            "testCreator1");
+
+        _configRepository.
+            QueryConfigsAsync(Arg.Any<ConfigQuery>(), Arg.Any<CancellationToken>())
+            .Returns(AsyncEnumerable.Empty<ConfigItem>());
+
+        _configRepository.AddOrUpdateConfigAsync(item, Arg.Any<CancellationToken>())
+            .Returns(item with { Id = 1 });
+
+        // Act
+        await _configService.SetConfigAsync(item, CancellationToken.None);
+
+        // Assert
+        await _configRepository.Received(1).AddOrUpdateConfigAsync(item, Arg.Any<CancellationToken>());
+        await _configHistoryRepository.Received(1).AddRecordAsync(
+            Arg.Is<HistoryItem>(h =>
+                h.ConfigId == 1 &&
+                h.Operation == ConfigHistoryKind.Created &&
+                h.OldValue == "none" &&
+                h.NewValue == "value1"),
+            Arg.Any<CancellationToken>());
+    }
+
+    [Fact]
+    public async Task SetConfigAsync_ShouldUpdateConfigConfigAndHistory_WhenExistingConfigUpdated()
+    {
+        // Arrange
+        ConfigItem existingItem = new(
+            Id: default,
+            "existingKey",
+            "oldValue",
+            "TestProject1",
+            "test1",
+            [ConfigEnvironment.Global],
+            DateTime.Today,
+            DateTime.Today,
+            "testCreator1");
+        ConfigItem updatedItem = new(
+            Id: default,
+            "existingKey",
+            "newValue",
+            "TestProject1",
+            "test1",
+            [ConfigEnvironment.Global],
+            DateTime.Today,
+            DateTime.Today,
+            "testCreator1");
+
+        _configRepository.
+            QueryConfigsAsync(Arg.Any<ConfigQuery>(), Arg.Any<CancellationToken>())
+            .Returns(new[] { existingItem }.ToAsyncEnumerable());
+
+        _configRepository.AddOrUpdateConfigAsync(updatedItem, Arg.Any<CancellationToken>())
+            .Returns(updatedItem with { Id = 1 });
+
+        // Act
+        await _configService.SetConfigAsync(updatedItem, CancellationToken.None);
+
+        // Assert
+        await _configRepository.Received(1).AddOrUpdateConfigAsync(updatedItem, Arg.Any<CancellationToken>());
+        await _configHistoryRepository.Received(1).AddRecordAsync(
+            Arg.Is<HistoryItem>(h =>
+                h.ConfigId == 1 &&
+                h.Operation == ConfigHistoryKind.Updated &&
+                h.OldValue == "oldValue" &&
+                h.NewValue == "newValue"),
+            Arg.Any<CancellationToken>());
+    }
+
+    [Fact]
+    public async Task DeleteConfigAsync_DeletesConfigAndRecordHistory_WhenConfigExists()
+    {
+        // Arrange
+        var request = new DeleteConfig.Request(
+            "existingKey",
+            "TestProject1",
+            "test1",
+            ConfigEnvironment.Global,
+            "testCreator1");
+        ConfigItem existingItem = new(
+            Id: 1,
+            "existingKey",
+            "oldValue",
+            "TestProject1",
+            "test1",
+            [ConfigEnvironment.Global],
+            DateTime.Today,
+            DateTime.Today,
+            "testCreator1");
+
+        _configRepository
+            .QueryConfigsAsync(Arg.Any<ConfigQuery>(), Arg.Any<CancellationToken>())
+            .Returns(new[] { existingItem }.ToAsyncEnumerable());
+
+        _configRepository.DeleteConfigAsync(
+                request.Namespace,
+                request.Profile,
+                request.Environment,
+                request.Key,
+                Arg.Any<CancellationToken>())
+            .Returns(1);
+
+        // Act
+        DeleteConfig.Result result = await _configService.DeleteConfigAsync(request, CancellationToken.None);
+
+        // Assert
+        result.Should().BeOfType<DeleteConfig.Result.Success>();
+
+        await _configRepository.Received(1).DeleteConfigAsync(
+            request.Namespace,
+            request.Profile,
+            request.Environment,
+            request.Key,
+            Arg.Any<CancellationToken>());
+
+        await _configHistoryRepository.Received(1).AddRecordAsync(
+            Arg.Is<HistoryItem>(h =>
+                h.ConfigId == 1 &&
+                h.Operation == ConfigHistoryKind.Deleted &&
+                h.OldValue == "oldValue" &&
+                h.NewValue == "none"),
+            Arg.Any<CancellationToken>());
+    }
+
+    [Fact]
+    public async Task DeleteConfigAsync_ReturnsNotFound_WhenConfigDoesNotExist()
+    {
+        // Arrange
+        var request = new DeleteConfig.Request(
+            "unknownKey",
+            "TestProject1",
+            "test1",
+            ConfigEnvironment.Global,
+            "testCreator1");
+
+        _configRepository
+            .QueryConfigsAsync(Arg.Any<ConfigQuery>(), Arg.Any<CancellationToken>())
+            .Returns(AsyncEnumerable.Empty<ConfigItem>());
+
+        // Act
+        DeleteConfig.Result result = await _configService.DeleteConfigAsync(request, CancellationToken.None);
+
+        // Assert
+        result.Should().BeOfType<DeleteConfig.Result.ConfigNotFound>();
+
+        await _configRepository.DidNotReceive().DeleteConfigAsync(
+            Arg.Any<string>(),
+            Arg.Any<string>(),
+            Arg.Any<ConfigEnvironment>(),
+            Arg.Any<string>(),
+            Arg.Any<CancellationToken>());
+    }
+
+    [Fact]
+    public async Task SetConfigsBatchAsync_ShouldFlattenJsonAndCallSetConfigForEachProperty()
+    {
+        // Arrange
+        string json = """
+        {
+            "Db": {
+                "Host": "localhost",
+                "Port": 5432
+            },
+        "Tags": ["a", "b"]
+        }
+        """;
+        JsonElement jsonElement = JsonSerializer.Deserialize<JsonElement>(json);
+
+        var request = new SetConfigsBatch.Request(
+            jsonElement,
+            "TestProject1",
+            "test",
+            ConfigEnvironment.Global,
+            "testCreator1");
+
+        _configRepository
+            .QueryConfigsAsync(Arg.Any<ConfigQuery>(), Arg.Any<CancellationToken>())
+            .Returns(AsyncEnumerable.Empty<ConfigItem>());
+
+        _configRepository.AddOrUpdateConfigAsync(Arg.Any<ConfigItem>(), Arg.Any<CancellationToken>())
+            .Returns(x => x.Arg<ConfigItem>());
+
+        // Act
+        await _configService.SetConfigsBatchAsync(request, CancellationToken.None);
+
+        // Assert
+        await _configRepository.Received(4).AddOrUpdateConfigAsync(Arg.Any<ConfigItem>(), Arg.Any<CancellationToken>());
+
+        await _configRepository.Received().AddOrUpdateConfigAsync(
+            Arg.Is<ConfigItem>(c => c.Key == "Db:Host" && c.Value == "localhost"), Arg.Any<CancellationToken>());
+
+        await _configRepository.Received().AddOrUpdateConfigAsync(
+            Arg.Is<ConfigItem>(c => c.Key == "Db:Port" && c.Value == "5432"), Arg.Any<CancellationToken>());
+
+        await _configRepository.Received().AddOrUpdateConfigAsync(
+            Arg.Is<ConfigItem>(c => c.Key == "Tags:0" && c.Value == "a"), Arg.Any<CancellationToken>());
+    }
+
+    [Fact]
+    public async Task QueryConfigsAsync_ShouldReturnRepositoryResult()
+    {
+        // Arrange
+        var query = new ConfigQuery([], "ns", "prof", ConfigEnvironment.Global, 10);
+        ConfigItem[] expectedItems =
+        [
+            new ConfigItem(
+                Id: 1,
+                "key1",
+                "value1",
+                "TestProject1",
+                "test1",
+                [ConfigEnvironment.Global],
+                DateTime.Today,
+                DateTime.Today,
+                "testCreator1"),
+
+            new ConfigItem(
+                Id: 2,
+                "key2",
+                "value2",
+                "TestProject1",
+                "test1",
+                [ConfigEnvironment.Global],
+                DateTime.Today,
+                DateTime.Today,
+                "testCreator1")
+        ];
+
+        _configRepository.QueryConfigsAsync(query, Arg.Any<CancellationToken>())
+            .Returns(expectedItems.ToAsyncEnumerable());
+
+        // Act
+        IAsyncEnumerable<ConfigItem> result = _configService.QueryConfigsAsync(query, CancellationToken.None);
+        List<ConfigItem> list = await result.ToListAsync();
+
+        // Assert
+        list.Should().BeEquivalentTo(expectedItems);
+    }
+}
